4e14aaabd60140d3c1415c7461447b4c14be1100
Katarzyna Beszta <katarzyna.beszta@gmail.com>
2020-07-08 17:05:03 +0200

    Add script.js

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+const correctAnswers = ['C', 'C', 'A', 'B', 'C'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+    // check answers 
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]) { 
+            score += 20;
+        }
+    });
+
+    // show result on the top of page 
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}`;
+        if (output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
+});
